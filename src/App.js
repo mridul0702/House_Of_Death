e767daf088b0bb2f91187dbe424bd9175ccb6650
mridul0702 <guptamridul0207@gmail.com>
2023-04-11 00:33:58 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+    <Navbar/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/complaint" element={<ComplaintForm />} />
       </Routes>
-    <div className="App">
-    <Navbar/>
     </div>
-  );
+  )
 }
 
 export default App;
