9719795a74a5c560268d6be07e73e2cded471c20
mridul0702 <guptamridul0207@gmail.com>
2023-04-11 00:37:54 +0530

    added Navbar + tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ComplaintForm from './components/ComplaintForm'
 function App() {
   return (
     <div className='App'>
